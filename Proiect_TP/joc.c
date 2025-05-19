991205283d766f473f2d3b397b94123585f60e23
AchiLara <lara.achi@student.upt.ro>
2025-05-19 23:48:40 +0300

    Update joc.c

--- Proiect_TP/joc.c
+++ Proiect_TP/joc.c
@@ -1,87 +1,86 @@
 #include <stdio.h>
-#include <windows.h>  // Pentru functia Sleep
+#include <stdlib.h>
+#include <windows.h>
 #include "joc.h"
 
-char tabla[N][N]; // Tabla de joc
+char tabla[N][N]; // Tabla globala de dimensiune NxN
 
 // Initializeaza tabla cu spatii libere
-void initializeazaTabla() 
-{
+void initializeazaTabla() {
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             tabla[i][j] = ' ';
 }
 
 // Afiseaza tabla de joc cu linii de separare
-void afiseazaTabla() 
-{
+void afiseazaTabla() {
     printf("\n   ");
-    for (int j = 0; j < N; j++) 
-        printf("%d   ", j + 1); // Afiseaza numarul coloanei
+    for (int j = 0; j < N; j++) {
+        printf(" %2d ", j + 1); // Afiseaza numarul coloanei
+    }
     printf("\n");
     
-    for (int i = 0; i < N; i++) 
-    {
-        printf("%d ", i + 1); // Afiseaza numarul liniei
-        for (int j = 0; j < N; j++) 
-        {
-            printf(" %c ", tabla[i][j]); 
-            if (j < N-1) printf("|"); 
+    // Corpul tablei
+    for (int i = 0; i < N; i++) {
+        // Linia cu simboluri
+        printf("%2d ", i + 1); // Numerotare randuri cu 2 cifre
+        for (int j = 0; j < N; j++) {
+            printf(" %c ", tabla[i][j]);
+            if (j < N - 1) printf("|");
         }
         printf("\n");
-        if (i < N-1) 
-        {
-            printf("  ---+---+---\n"); 
+        
+        // Linia de separare (doar intre randuri)
+        if (i < N - 1) {
+            printf("   ");
+            for (int j = 0; j < N; j++) {
+                printf("---");
+                if (j < N - 1) printf("+");
+            }
+            printf("\n");
         }
     }
     printf("\n");
 }
 
-int verificaCastigator(char simbol) 
-{
-    // Verifica linii
-    for (int i = 0; i < N; i++) 
-    {
-        if (tabla[i][0] == simbol && tabla[i][1] == simbol && tabla[i][2] == simbol) 
-            return 1;
-    }
+int verificaCastigator(char simbol) {
+    int castiga_linie, castiga_coloana;
     
-    // Verifica coloane
-    for (int j = 0; j < N; j++) 
-    {
-        if (tabla[0][j] == simbol && tabla[1][j] == simbol && tabla[2][j] == simbol) 
-            return 1;
+    // Verifica linii si coloane
+    for (int i = 0; i < N; i++) {
+        castiga_linie = 1;
+        castiga_coloana = 1;
+        for (int j = 0; j < N; j++) {
+            if (tabla[i][j] != simbol) castiga_linie = 0;
+            if (tabla[j][i] != simbol) castiga_coloana = 0;
+        }
+        if (castiga_linie || castiga_coloana) return 1;
     }
     
     // Verifica diagonale
-    if (tabla[0][0] == simbol && tabla[1][1] == simbol && tabla[2][2] == simbol) 
-        return 1;
-    if (tabla[0][2] == simbol && tabla[1][1] == simbol && tabla[2][0] == simbol) 
-        return 1;
+    int castiga_diag1 = 1, castiga_diag2 = 1;
+    for (int i = 0; i < N; i++) {
+        if (tabla[i][i] != simbol) castiga_diag1 = 0;
+        if (tabla[i][N-1-i] != simbol) castiga_diag2 = 0;
+    }
     
-    return 0;
+    return castiga_diag1 || castiga_diag2;
 }
 
-// Verifica daca tabla este plina 
 int esteRemiza() {
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
-            if (tabla[i][j] == ' ') 
-                return 0;
+            if (tabla[i][j] == ' ') return 0;
     return 1;
 }
 
-// Verifica daca o mutare este valida
-int mutareValida(int linie, int coloana) 
-{
+int mutareValida(int linie, int coloana) {
     return (linie >= 0 && linie < N && 
             coloana >= 0 && coloana < N && 
             tabla[linie][coloana] == ' ');
 }
 
-// Plaseaza un simbol pe tabla
-void seteazaMutare(int linie, int coloana, char simbol) 
-{
+void seteazaMutare(int linie, int coloana, char simbol) {
     tabla[linie][coloana] = simbol;
 }
 
